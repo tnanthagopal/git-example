c949bcfef12ef78ba4daaee3732f05d212330335
Ruby <t.nanthagopal.com>
2021-10-22 09:07:53 +0530

    First edit in main

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>First edit in dev_branch2</p>
+        <p>First edit in main</p>
         <a
           className="App-link"
           href="https://reactjs.org"
